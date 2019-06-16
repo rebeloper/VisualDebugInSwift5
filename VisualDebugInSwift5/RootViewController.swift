75eb091f8318399e143667ced2bd208b28657e4d
Alex <therebeloper@gmail.com>
2019-06-16 13:21:37 +0300

    first commit

--- VisualDebugInSwift5/RootViewController.swift
+++ VisualDebugInSwift5/RootViewController.swift
@@ -6,15 +6,38 @@
 //  Copyright © 2019 Alex Nagy. All rights reserved.
 //
 
-import UIKit
+import TinyConstraints
 
 class RootViewController: UIViewController {
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        label.text = "Hello Visual Debug"
+        label.debug()
+        
+        view.addSubview(label)
+        label.centerInSuperview()
+        
+        label.leftToSuperview(offset: 32)
+        label.rightToSuperview(offset: -32)
     }
 
+}
 
+extension UIView {
+    @discardableResult
+    public func debug() -> UIView {
+        let myBlue = UIColor(red: 42/255, green: 126/255, blue: 254/255, alpha: 1.0)
+        backgroundColor = myBlue.withAlphaComponent(0.3)
+        layer.borderColor = myBlue.cgColor
+        layer.borderWidth = 1.5
+        layer.cornerRadius = 3
+        layer.masksToBounds = true
+        return self
+    }
 }
 
