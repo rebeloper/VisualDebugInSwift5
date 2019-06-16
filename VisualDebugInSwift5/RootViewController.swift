c1af9af774f5ab488f93ecb8d17783aebe90a9c4
Alex <therebeloper@gmail.com>
2019-06-16 12:42:58 +0300

    Initial Commit

---
+++ VisualDebugInSwift5/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  VisualDebugInSwift5
+//
+//  Created by Alex Nagy on 16/06/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
